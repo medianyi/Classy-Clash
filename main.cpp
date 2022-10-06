a0a4f0d9570164a966779dbc5604e5f743cbaea4
unknown <vladimirmedianyi@gmail.com>
2022-10-06 19:00:41 +0300

    animated the Character

--- main.cpp
+++ main.cpp
@@ -16,11 +16,31 @@ int main()
 
     float speed{4.0};
 
+    Texture2D knight_run = LoadTexture("./characters/knight_run_spritesheet.png");
+    Texture2D knight_idle = LoadTexture("./characters/knight_idle_spritesheet.png");
+    Texture2D knight = LoadTexture("./characters/knight_idle_spritesheet.png");
+    Vector2 knightPos{
+        windowDimension[0]/2.0f - 4.0f * (0.5f * (float)knight.width/6.0f),
+        windowDimension[1]/2.0f - 4.0f * (0.5f * (float)knight.height)
+    };
+    
+
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1.f};
+
+    // animation variable
+    float runningTime{};
+    int frame{};
+    const int maxFrame{6};
+    const float updateTime{1.f/12.f};
+
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
+ 
 
         Vector2 direction{};
         if (IsKeyDown(KEY_A)) direction.x -= speed;
@@ -30,11 +50,39 @@ int main()
         if (Vector2Length(direction) != 0.0)
         {   
             // set mapPos = mapPos-direction
-            
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+            knight = knight_run;
+        }
+        else
+        {
+            knight = knight_idle;
         }
         
+        // draw the map 
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+
+        // update animation frame
+        runningTime += GetFrameTime();
+        if (runningTime >= updateTime)
+        {
+            frame++;
+            runningTime = 0.f;
+            if (frame > maxFrame)
+            {
+                frame = 0;
+            }
+            
+        }
+        
+
+        // draw the character
+        Rectangle source{frame * (float)knight.width/6.f, 0.f, rightLeft * (float)knight.width/6.f, (float)knight.height}; 
+        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6, 4.0f * (float)knight.height};
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
+
+        
+
 
         EndDrawing();
     }
